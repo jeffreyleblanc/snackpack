1324e50fef406b1b90dbd25cdc729507fb763431
Jeff LeBlanc <jeffreyleblanc@users.noreply.github.com>
2022-07-11 10:20:14 -0400

    Adding better toml parsing.

--- experiments/test-toml.py
+++ experiments/test-toml.py
@@ -1,11 +1,27 @@
 #! /usr/bin/env python3
 
 import toml
-import json
 
-with open('test.toml','r') as f:
-    obj = toml.load(f)
+def load_toml_config(filepath, clean=True):
+    with open(filepath,'r') as f:
+        obj = toml.load(f)
 
+    for src in obj['sources']:
+        print(src)
+        if 'sources__ARR' in src:
+            lst = []
+            for l in src['sources__ARR'].splitlines():
+                l = l.strip()
+                if l != '' and not l.startswith('#'):
+                    lst.append(l)
+            src['sources'] = lst
+            if clean:
+                del src['sources__ARR']
+    return obj
 
-print(json.dumps(obj,indent=4))
+if __name__ == '__main__':
+    fp = 'test.toml'
+    obj = load_toml_config(fp)
+    import json
+    print(json.dumps(obj,indent=4))
 
